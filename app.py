4c42cae881a8477d86f8552f0cb72845a1dbd092
g5g5k6 <a0860191@gmail.com>
2025-02-22 00:19:21 +0800

    Update app.py and add new files create_db.py and db.py

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 # -*- coding: utf-8 -*-
 from flask_cors import CORS
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import json
 import os
+from db import add_word_group, get_due_groups, review_word_group,check_review,create_word_group()
 
 app = Flask(__name__)
-CORS(app)  
-current_path = os.getcwd()
-project_root = os.path.dirname(current_path)
-json_path = os.path.join(project_root, "json", "dic.json")
 
-def load_words():
-    try:
-        with open(json_path, 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return ["111"]  # 若檔案不存在，回傳空列表
+# @app.route("/add_group", methods=["POST"])
+# def add_group():
+    # data = request.json
+    # words = data.get("words", [])
+# 
+    # if not words or len(words) != 10:
+        # return jsonify({"error": "10 個單字一組"}), 400
+# 
+    # add_word_group(words)
+    # return jsonify({"message": "單字組已新增"})
 
-words_data = load_words()
+@app.route("/due_groups", methods=["GET"])
+#自動載入
+def due_groups():
+    print("嘗試get單字")
+    groups = get_due_groups()
+    return jsonify(groups)
 
-@app.route('/api/words', methods=['GET'])
+@app.route("/review/<int:group_id>", methods=["POST"])
+#手動按鈕
+def review(group_id):
+    result = review_word_group(group_id)
+    return jsonify({"message": result})
 
-def get_words():
-    return jsonify(words_data), 200, {'Content-Type': 'application/json; charset=utf-8'}
+@app.route("/check_review", methods=["POST"])
+#自動更新
+def check():
+    check_review()
+    return jsonify({"message": "Success"})
 
-if __name__ == '__main__':
+@app.route("/create_word_group", methods=["POST"])
+def create_group():
+    result = create_word_group()
+    return jsonify({"message": result})
+
+
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+
